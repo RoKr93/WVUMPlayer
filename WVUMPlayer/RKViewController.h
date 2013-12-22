6949d8bf0acbbf441df1931fe8e42fb7cfb7aab7
Roshan Krishnan <r.krishnan1@umiami.edu>
2013-12-22 00:12:36 -0500

    add all files

--- WVUMPlayer/RKViewController.h
+++ WVUMPlayer/RKViewController.h
@@ -7,7 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import "Reachability.h"
+#import <SystemConfiguration/SystemConfiguration.h>
 
-@interface RKViewController : UIViewController
+@interface RKViewController : UIViewController{
+    IBOutlet UIButton *playMusic;
+    IBOutlet UIButton *stopMusic;
+    IBOutlet UIButton *infoButton;
+    IBOutlet UILabel *currentInfo;
+    MPMoviePlayerController *player;
+    
+    BOOL *isPlaying;
+}
+-(IBAction)startButtonPressed:(id)sender;
+-(IBAction)stopButtonPressed:(id)sender;
+-(IBAction)infoButtonPressed:(id)sender;
+-(void)loadCurrentInfo;
+-(BOOL)connected;
 
 @end
--- WVUMPlayer/RKViewController.m
+++ WVUMPlayer/RKViewController.m
@@ -18,12 +18,104 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    isPlaying = NO;
+    player = [[MPMoviePlayerController alloc] initWithContentURL:[NSURL URLWithString:@"http://www.wvum.org/listen.pls"]];
+    player.movieSourceType = MPMovieSourceTypeStreaming;
+    player.view.hidden = YES;
+    player.useApplicationAudioSession = NO;
+    [self.view addSubview:player.view];
+    currentInfo.numberOfLines = 0;
+    CGSize labelSize = [currentInfo.text sizeWithFont:currentInfo.font
+                              constrainedToSize:currentInfo.frame.size
+                                  lineBreakMode:currentInfo.lineBreakMode];
+    currentInfo.frame = CGRectMake(
+                             currentInfo.frame.origin.x, currentInfo.frame.origin.y,
+                             currentInfo.frame.size.width, labelSize.height);
+    
+    //check for internet connection
+    if(![self connected]){
+        //if no connection, display warning message.
+        NSString *message = @"Your iPhone does not have a network connection. Please try again later.";
+        
+        UIAlertView *info = [[UIAlertView alloc]
+                             initWithTitle:@"Warning" message:message delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [info show];
+    }else{
+        //if connected, get info from web.
+        [self loadCurrentInfo];
+        [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(loadCurrentInfo) userInfo:nil repeats:YES];
+    }
+    
+}
+-(void)loadCurrentInfo{
+    //grab HTML code from web player.
+    NSError *error = nil;
+    NSString *html = [NSString stringWithContentsOfURL:[NSURL URLWithString:@"http://wvum.org/index.php/wvum/stream/"] encoding:NSASCIIStringEncoding error:&error];
+    
+    //get string that contains artist name and song name.
+    NSScanner *stringScanner = [NSScanner scannerWithString:html];
+    NSString *htmlContent = [[NSString alloc] init];
+    while ([stringScanner isAtEnd] == NO) {
+        [stringScanner scanUpToString:@"<!--includeThisInApp-->" intoString:nil];
+        [stringScanner scanUpToString:@"</" intoString:&htmlContent];
+    }
+    htmlContent = [htmlContent substringFromIndex:23];
+
+    //replace irregular HTML characters.
+    NSMutableString *printedContent = [[NSMutableString alloc] init];
+    for(int i = 0; i < htmlContent.length; i++){
+        char c = [htmlContent characterAtIndex:i];
+        NSString *firstString = [NSString stringWithFormat:@"%c", c];
+        NSString *compString = [NSString stringWithFormat:@"â"];
+        if([firstString isEqualToString:compString]){
+            [printedContent appendString:@"'"];
+        }else{
+            NSString *s = [NSString stringWithFormat:@"%c", c];
+            [printedContent appendString:s];
+        }
+    }
+    
+    currentInfo.text = printedContent;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)startButtonPressed:(id)sender{
+    //if not already playing, begin streaming.
+    if([self connected] && !isPlaying){
+        currentInfo.text = @"loading...";
+        isPlaying = YES;
+        [player play];
+        [self loadCurrentInfo];
+    }
+}
+
+-(IBAction)stopButtonPressed:(id)sender{
+    //if already playing, stop streaming.
+    if([self connected] && isPlaying){
+        isPlaying = NO;
+        [player stop];
+    }
+}
+
+-(IBAction)infoButtonPressed:(id)sender{
+    //display information.
+    NSString *message = @"WVUM 90.5 is the voice of the University of Miami. Check us out at www.wvum.org.";
+
+    UIAlertView *info = [[UIAlertView alloc]
+                         initWithTitle:@"Information" message:message delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [info show];
+}
+
+-(BOOL)connected{
+    //check for internet connection using Tony Million's Reachability.h/.m, return YES or NO accordingly.
+    Reachability *reachability = [Reachability reachabilityForInternetConnection];
+    NetworkStatus networkStatus = [reachability currentReachabilityStatus];
+    return !(networkStatus == NotReachable);
 }
 
 @end
